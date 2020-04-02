3a88d516b5fd5de86c0fc60d0dd5df97cb03e6de
Tom Banish <tombanish@gmail.com>
2020-04-02 19:22:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,10 +6,10 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   
-  @@artists = []
+  @@all = []
 
   def initialize
-    @@artists << self
+    @@all << self
     @songs = []
   end
 
@@ -18,7 +18,7 @@ class Artist
   # end
 
   def self.all
-    @@artists
+    @@all
   end
 
   def add_song(song)
