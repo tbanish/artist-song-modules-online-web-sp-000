6b5b98055b91e76f8d847a0472f57a0cff6d789f
Tom Banish <tombanish@gmail.com>
2020-04-02 19:24:08 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,9 @@
 module Findable
   module ClassMethods
-    @@all = []
+    @all = []
     
     def find_by_name(name)
-      @@all.detect{|a| a.name == name}
+      @all.detect{|a| a.name == name}
     end
     
   end
