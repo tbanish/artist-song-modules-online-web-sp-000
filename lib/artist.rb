9be9048b4a17c4ed73489b1f1bd6ade6844de011
Tom Banish <tombanish@gmail.com>
2020-04-02 19:08:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,10 @@
 require 'pry'
-#require_relative '../lib/concerns/memorable'
 
 class Artist
-  extend Memorable::ClassMethods
-  
   attr_accessor :name
   attr_reader :songs
+  extend Memorable::ClassMethods
+  
   
   @@artists = []
 
